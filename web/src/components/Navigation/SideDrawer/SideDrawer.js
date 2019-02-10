8da370f7f78af6969d1f73050ef51b7b3b0dc380
mor <morex48562@gmail.com>
2019-02-10 20:57:44 +0200

    add side drawer

--- web/src/components/Navigation/SideDrawer/SideDrawer.js
+++ web/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import Logo from '../../UI/Logo/Logo'
+import Style from './SideDrawer.module.css';
+import NavItems from '../NavItems/NavItems'
 
+const css={
+    height:'180%',
+    display:'flex',
+    flexFlow:'column',
+    listStyle: 'none',
+    justifyContent: 'space-between',
+    padding:'0'
+}
 const sideDrawer = (props)=>{
-    <Logo/>
+    let classes = [Style.Container,Style.Close]
+  if(!props.isClose)
+{
+    classes = [Style.Container,Style.Open];
+}
+return(
+<div className={classes.join(' ')}>
+      <div className='row mt-5'>   
+     <div id='main' className='col'>
+        <NavItems pageNum={props.pageNum} class={css}/>
+
+     </div> 
+    </div>
+</div>
+);
+  
+
 }
 
 export default sideDrawer
